4d4f0b224b8064d6225bfc18ca577602e8b20150
g-cushen <20012812@mail.wit.ie>
2020-05-07 21:42:51 +0100

    Gcushen CA App

---
+++ app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
@@ -0,0 +1,23 @@
+package org.wit.placemark.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+interface PlacemarkStore {
+    fun findAll(): List<PlacemarkModel>
+    fun create(placemark: PlacemarkModel)
+    fun update(placemark: PlacemarkModel)
+}
+@Parcelize
+data class Location(var lat: Double = 0.0,
+                    var lng: Double = 0.0,
+                    var zoom: Float = 0f) : Parcelable
+@Parcelize
+data class PlacemarkModel(var id: Long = 0,
+                          var title: String = "",
+                          var description: String = "",
+                          var image: String = "",
+                          var lat : Double = 0.0,
+                          var lng: Double = 0.0,
+                          var zoom: Float = 0f) : Parcelable
+
